aeca61b5b6898c2f31a647366de9a605a6c92943
Chris Ellis <cellis@iqms.com>
2017-08-10 12:58:28 -0700

    Basic home page routing implemented

--- what-the-f-should-i-wear/src/app/app.module.ts
+++ what-the-f-should-i-wear/src/app/app.module.ts
@@ -5,25 +5,24 @@ import { NglModule } from 'ng-lightning';
 import { HomeComponent } from './modules/home/home.component';
 import { ClosetComponent } from './modules/closet/closet.component';
 import { LaundryComponent } from './modules/laundry/laundry.component';
-import { WardrobeAssComponent } from './modules/wardrobe-ass/wardrobe-ass.component';
+//import { WardrobeAssComponent } from './modules/wardrobe-ass/wardrobe-ass.component';
 
 import { AppComponent } from './app.component';
 
 
-const appRouters: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'my-closet', component: ClosetComponent },
-  { path: 'laundry', component: LaundryComponent },
-  { path: 'wardrobe-assistant', component: WardrobeAssComponent }
-];
+// const appRouters: Routes = [
+//   { path: 'home', component: HomeComponent },
+//   { path: 'my-closet', component: ClosetComponent },
+//   { path: 'laundry', component: LaundryComponent },
+//   { path: 'wardrobe-assistant', component: WardrobeAssComponent }
+// ];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ClosetComponent,
-    LaundryComponent,
-    WardrobeAssComponent
+    LaundryComponent
   ],
   imports: [
     BrowserModule,
